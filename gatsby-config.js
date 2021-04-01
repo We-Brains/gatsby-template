74043f3070ecd13a7b4fcef94cbc5a1fa29b4a8f
moon <mmesyatz@gmail.com>
2021-04-01 14:51:48 +0300

    add: template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,60 +1,62 @@
-'use strict'
+/* eslint-disable  */
+require('dotenv').config();
 
 module.exports = {
-  siteMetadata: {
-    title: 'gatsby-starter-typescript-plus',
-    description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
-    keywords: 'gatsbyjs, gatsby, javascript, sample, something',
-    siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
-    author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
-      email: 'resir014@gmail.com'
-    }
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'content',
-        path: `${__dirname}/src/content`
-      }
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1rem'
-            }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1140,
-              quality: 90,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
-    },
-    'gatsby-transformer-json',
-    {
-      resolve: 'gatsby-plugin-canonical-urls',
-      options: {
-        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
-      }
-    },
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet'
-  ]
-}
+	siteMetadata: {
+		author: {
+			name: 'Bla'
+		}
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				background_color: '#323232',
+				theme_color: '#323232',
+				display: 'minimal-ui',
+				icon: 'assets/images/fav.png'
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-layout',
+			options: {
+				component: require.resolve('./src/components/layout.tsx')
+			}
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'src',
+				path: `${__dirname}/src`
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-eslint',
+			options: {
+				test: /\.ts$|\.tsx$/,
+				exclude: /(node_modules|.cache|public|)/,
+				stages: [ 'develop' ],
+				options: {
+					emitWarning: true,
+					failOnError: true
+				}
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-react-svg',
+			options: {
+				rule: {
+					include: /\/assets\/.*.svg/
+				}
+			}
+		},
+		'gatsby-plugin-sass',
+		'gatsby-plugin-typescript',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-react-helmet'
+	]
+};
